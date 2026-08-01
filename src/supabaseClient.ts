d195923527ae3d8b097b1bc7fb21905e129052c2
Willsonraiii <leonalxoox@gmail.com>
2026-08-01 12:10:14 +0545

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dzebbrsougjsypdejfkg.supabase.co';
-const supabaseKey = 'sb_publishable_Wp2Iok3D-HxD5fQdwfFwSA_cjtkFx0U';
+const supabaseUrl = 'https://tcyizrfuqoevcechalix.supabase.co';
+const supabaseKey = 'sb_publishable_p_XNCIvqRPM_rrnCqTY75A_xTYFhk-u';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
